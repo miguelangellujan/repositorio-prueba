850370417ec4058e0c2d72295ffcd9baa274df42
miguelangellujan <miguel.lujan@edu.uah.es>
2024-02-08 20:10:44 +0100

    primer commit

--- src/Concatenaciones.java
+++ src/Concatenaciones.java
@@ -8,11 +8,9 @@ public class Concatenaciones {
         while (System.currentTimeMillis() < endTime) {
             concatenaciones++;
             System.out.printf("%8d, %4d segundos\n", concatenaciones, (System.currentTimeMillis() - startTime) / 1000);
-            // Concatenar cadena con el operador +
             String resultado = pCadena + concatenaciones;
         }
     }
-    // Método para realizar la concatenación usando StringBuilder
     public static void testLista(int pTiempo, String pCadena) {
         System.out.println("Ejecutando testLista...");
 
@@ -20,27 +18,25 @@ public class Concatenaciones {
         long endTime = startTime + (pTiempo * 1000);
         int concatenaciones = 0;
 
-        // Usar StringBuilder para realizar la concatenación eficientemente
         StringBuilder builder = new StringBuilder();
 
         while (System.currentTimeMillis() < endTime) {
             concatenaciones++;
             System.out.printf("%8d, %4d segundos\n", concatenaciones, (System.currentTimeMillis() - startTime) / 1000);
-            // Concatenar cadena usando StringBuilder
             builder.append(pCadena).append(concatenaciones);
         }
     }
 
     public static void main(String[] args) {
-        int tiempo = 5; // Ejecutar durante 5 segundos
+        int tiempo = 10;
         String cadena = "Hola";
 
-        // Ejecutar el primer método
-        System.out.println("\n*** Test usando operador + ***");
+        // primer método
+        System.out.println("\n Test usando operador +");
         testConcatenar(tiempo, cadena);
 
-        // Ejecutar el segundo método
-        System.out.println("\n*** Test usando StringBuilder ***");
+        // segundo método
+        System.out.println("\n Test usando StringBuilder ");
         testLista(tiempo, cadena);
     }
 }
